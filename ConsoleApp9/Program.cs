a0bc82bd5b00ecd21d7ef89922883b1b140d3bcf
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-04-27 16:29:02 +0530

    Generics

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp9
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Create a list of 5 strings.
+        var animals = new List<string>() { "bird", "dog","Camel","Lion","Elephant" };
+        // Insert strings from an array in position 1.
+        animals.InsertRange(1, new string[] { "frog", "snake" });
+        foreach (string value in animals)
         {
+            Console.WriteLine("RESULT: " + value);
         }
     }
 }
